b320ca2d84c846b66670e143a33046e2752b276a
Uyiekpen Osazie Elizabeth <71497866+uyiekpen@users.noreply.github.com>
2023-10-24 01:02:14 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+
+ // Add basePath
+  basePath: '/weddingInvite',
+
+}
 
 module.exports = nextConfig
